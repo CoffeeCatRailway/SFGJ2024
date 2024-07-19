101d8e4229b35169ce0b91a70cd184fd7a0244f3
CoffeeCatRailway <dragonlee1002@gmail.com>
2024-07-19 13:51:02 +1000

    door sound

--- scripts/elements/Door.gd
+++ scripts/elements/Door.gd
@@ -12,19 +12,26 @@ func _ready() -> void:
 	animPlayer.play("close", -1., 10.)
 	if locked:
 		lock(10.)
+	else:
+		unlock(10.)
+	
+	$Area2D.body_entered.connect(entered)
+	$Area2D.body_exited.connect(exited)
 
-func _on_area_2d_body_entered(_body: Node2D) -> void:
+func entered(_body: Node2D) -> void:
 	if !locked:# && body.name == "Player":
 		animPlayer.play("open", -1., speed)
+		$AudioStreamPlayer2D.play()
 
-func _on_area_2d_body_exited(_body: Node2D) -> void:
+func exited(_body: Node2D) -> void:
 	if !locked:# && body.name == "Player":
 		animPlayer.play("close", -1., speed)
+		$AudioStreamPlayer2D.play()
 
 func lock(_speed: float = speed) -> void:
 	locked = true
 	lockAnimPlayer.play("lock" + ("Purple" if isPurple else "Green"), -1., _speed)
 
-func unlock() -> void:
+func unlock(_speed: float = speed) -> void:
 	locked = false
-	lockAnimPlayer.play("unlock" + ("Purple" if isPurple else "Green"), -1., speed)
+	lockAnimPlayer.play("unlock" + ("Purple" if isPurple else "Green"), -1., _speed)
