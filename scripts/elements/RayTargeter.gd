5dc8a21a133d4be73b0bda5befa57100b4422d5a
CoffeeCatRailway <dragonlee1002@gmail.com>
2024-07-19 10:08:08 +1000

    ray target max distance

--- scripts/elements/RayTargeter.gd
+++ scripts/elements/RayTargeter.gd
@@ -2,6 +2,7 @@ class_name TargeterComponent
 extends RayCast2D
 
 @export var targetScript: GDScript
+@export var maxDistance: float = -1.
 
 var target: Node2D = null # Loki?
 
@@ -11,5 +12,8 @@ func _process(_delta: float) -> void:
 	if !target:
 		return
 	
-	if target.get_script() == targetScript && target.healthComponent.isDead:
+	var isDead: bool = target.get_script() == targetScript && target.healthComponent.isDead
+	var isOutOfRange: bool = maxDistance > 0. && target.global_position.distance_to(get_parent().global_position) > maxDistance
+	
+	if isDead || isOutOfRange:
 		target = null
