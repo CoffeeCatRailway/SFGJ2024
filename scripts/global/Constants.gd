07bd223c4d5a282ebc6957b5e5b77a1d31795c11
CoffeeCatRailway <dragonlee1002@gmail.com>
2024-08-04 21:00:33 +1000

    Remove old utils & contants

--- scripts/global/Constants.gd
+++ scripts/global/Constants.gd
@@ -1,24 +1,3 @@
 extends Node
 
-## Facing enum
-#enum Facing {
-	#UP, DOWN, LEFT, RIGHT
-#}
-
-## Tilemaps & Layers
-enum Layers {
-	WALLS = 0,
-	FLOOR = 1
-}
-
-enum Terrains {
-	FLOOR = 0
-}
-
-#const cropsAtlasId: int = 5
-#const soilAtlasId: int = 9
-#
-#const cropCornAtlasCoords: Vector2i = Vector2i(0, 0)
-#const cropCornGrowthStages: int = 5
-#
-#const soilHoedAtlasCoords: Vector2i = Vector2i(5, 6)
+## NOTE: Keeping class incase
--- scripts/global/Utils.gd
+++ scripts/global/Utils.gd
@@ -20,23 +20,3 @@ func tweenBounce(node: Node, x: float = .8, y: float = 1.4) -> void:
 
 func isMoveKeyPressed() -> bool:
 	return Input.is_action_pressed("move_up") || Input.is_action_pressed("move_down") || Input.is_action_pressed("move_left") || Input.is_action_pressed("move_right")
-
-## Tilemap
-func getCustomData(tilemap: TileMap, tilePos: Vector2i, layerName: String, layer: Constants.Layers) -> Variant:
-	var tileData: TileData = tilemap.get_cell_tile_data(layer, tilePos)
-	if tileData:
-		return tileData.get_custom_data(layerName)
-	return false
-
-func setCustomData(tilemap: TileMap, tilePos: Vector2i, layerName: String, layer: Constants.Layers, value: Variant) -> bool:
-	var tileData: TileData = tilemap.get_cell_tile_data(layer, tilePos)
-	if tileData:
-		tileData.set_custom_data(layerName, value)
-		return true
-	return false
-#
-func isTileEmpty(tilemap: TileMap, pos: Vector2i, layers: Array = [Constants.Layers.WALLS]) -> bool:
-	for layer: Constants.Layers in layers:
-		if tilemap.get_used_cells(layer).has(pos):
-			return false
-	return true
